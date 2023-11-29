dacdc7ca34b5f1983f95a216e64a914878be1dca
Imane ABDOU <imaneabdou29@gmail.com>
2023-11-29 12:58:46 +0100

    Create entity and test it

---
+++ src/main/java/fr/efrei/server/service/ItemService.java
@@ -0,0 +1,21 @@
+package fr.efrei.server.service;
+
+import fr.efrei.server.domain.Item;
+import fr.efrei.server.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+
+    public final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public List<Item> findAll() {
+        return itemRepository.findAll();
+    }
+}
